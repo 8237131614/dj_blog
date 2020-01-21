d52bdb784f5f01ac4b642164e7013991dbd460bb
shadaab <shadaab.maniyar7@gmail.com>
2020-01-21 11:35:42 +0530

    updated

--- pydjblog/settings.py
+++ pydjblog/settings.py
@@ -12,7 +12,6 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 
 import os
 
-
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -123,7 +122,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'ststicfiles')
 
 STATIC_URL = '/static/'
 
